e85d4693ab56192595b27a5ba65629bf35925f98
malenyab <maleny_1292@hotmail.com>
2017-03-11 17:26:47 -0600

    Add files via upload

---
+++ FuncionesVB/FuncionesVB/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+    Dim func As New funciones()
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim resultado As Double
+
+        resultado = suma(5, 15)
+
+        Label1.Text = "El resultado de la función en la misma clase es: " + resultado.ToString
+
+        Dim resultado2 As Double
+
+        resultado2 = func.multiplicacion(5, 15)
+
+        Label2.Text = "El resultado de la función en otra clase es: " + resultado2.ToString
+
+
+    End Sub
+
+    Public Function suma(valor1 As Double, valor2 As Double)
+        Dim sum As Double
+
+        sum = valor1 + valor2
+
+        Return sum
+    End Function
+
+End Class
---
+++ FuncionesVB/FuncionesVB/funciones.vb
@@ -0,0 +1,13 @@
+﻿Public Class funciones
+
+    Public Function multiplicacion(valor1 As Double, valor2 As Double) As Double
+
+        Dim resultado2 As Double
+
+        resultado2 = valor1 * valor2
+
+        Return resultado2
+
+    End Function
+
+End Class
